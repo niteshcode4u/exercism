65e5512eb004f1bcbfefb884235be5ee93e9b7e7
Nitesh Mishra <>
2023-04-28 16:33:23 +0530

    updated logic for log-level

--- lib/log_level.ex
+++ lib/log_level.ex
@@ -44,23 +44,32 @@ defmodule LogLevel do
   """
   @spec to_label(any, any) :: :debug | :error | :fatal | :info | :trace | :unknown | :warning
   def to_label(level, legacy?) do
+    label =
+      cond do
+        level == 0 -> :trace
+        level == 1 -> :debug
+        level == 2 -> :info
+        level == 3 -> :warning
+        level == 4 -> :error
+        level == 5 -> :fatal
+        true -> :unknown
+      end
+
     cond do
-      level == 0 and not legacy? -> :trace
-      level == 1 -> :debug
-      level == 2 -> :info
-      level == 3 -> :warning
-      level == 4 -> :error
-      level == 5 and not legacy? -> :fatal
-      true -> :unknown
+      label == :trace and legacy? -> :unknown
+      label == :fatal and legacy? -> :unknown
+      true -> label
     end
   end
 
   @spec alert_recipient(any, any) :: :dev1 | :dev2 | false | :ops
   def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+
     cond do
-      level in [0, 5, 6] and legacy? -> :dev1
-      level == 4 || (level == 5 and not legacy?) -> :ops
-      level == 6 and not legacy? -> :dev2
+      label == :error or label == :fatal -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown -> :dev2
       true -> false
     end
   end
