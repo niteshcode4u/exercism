08e176a94af8de7e5be67e28ea628f8c458c5bce
Nitesh Mishra <>
2023-04-28 11:47:36 +0530

    fixes failing tests

--- test/pacman_rules_test.exs
+++ test/pacman_rules_test.exs
@@ -1,76 +1,76 @@
-defmodule PacmanRulesTest do
+defmodule PacmanPacmanRulesTest do
   use ExUnit.Case
 
   describe "eat_ghost?/2" do
     @tag task_id: 1
     test "ghost gets eaten" do
-      assert Rules.eat_ghost?(true, true)
+      assert PacmanRules.eat_ghost?(true, true)
     end
 
     @tag task_id: 1
     test "ghost does not get eaten because no power pellet active" do
-      refute Rules.eat_ghost?(false, true)
+      refute PacmanRules.eat_ghost?(false, true)
     end
 
     @tag task_id: 1
     test "ghost does not get eaten because not touching ghost" do
-      refute Rules.eat_ghost?(true, false)
+      refute PacmanRules.eat_ghost?(true, false)
     end
 
     @tag task_id: 1
     test "ghost does not get eaten because no power pellet is active, even if not touching ghost" do
-      refute Rules.eat_ghost?(false, false)
+      refute PacmanRules.eat_ghost?(false, false)
     end
   end
 
   describe "score?/2" do
     @tag task_id: 2
     test "score when eating dot" do
-      assert Rules.score?(false, true)
+      assert PacmanRules.score?(false, true)
     end
 
     @tag task_id: 2
     test "score when eating power pellet" do
-      assert Rules.score?(true, false)
+      assert PacmanRules.score?(true, false)
     end
 
     @tag task_id: 2
     test "no score when nothing eaten" do
-      refute Rules.score?(false, false)
+      refute PacmanRules.score?(false, false)
     end
   end
 
   describe "lose?/2" do
     @tag task_id: 3
     test "lose if touching a ghost without a power pellet active" do
-      assert Rules.lose?(false, true)
+      assert PacmanRules.lose?(false, true)
     end
 
     @tag task_id: 3
     test "don't lose if touching a ghost with a power pellet active" do
-      refute Rules.lose?(true, true)
+      refute PacmanRules.lose?(true, true)
     end
 
     @tag task_id: 3
     test "don't lose if not touching a ghost" do
-      refute Rules.lose?(true, false)
+      refute PacmanRules.lose?(true, false)
     end
   end
 
   describe "win?/3" do
     @tag task_id: 4
     test "win if all dots eaten" do
-      assert Rules.win?(true, false, false)
+      assert PacmanRules.win?(true, false, false)
     end
 
     @tag task_id: 4
     test "don't win if all dots eaten, but touching a ghost" do
-      refute Rules.win?(true, false, true)
+      refute PacmanRules.win?(true, false, true)
     end
 
     @tag task_id: 4
     test "win if all dots eaten and touching a ghost with a power pellet active" do
-      assert Rules.win?(true, true, true)
+      assert PacmanRules.win?(true, true, true)
     end
   end
 end
