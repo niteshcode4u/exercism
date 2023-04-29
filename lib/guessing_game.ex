5c38da6e449f93f2165b57e9f6cf19a629ecb008
Nitesh Mishra <>
2023-04-29 10:37:30 +0530

    Add logic for guessing-game exercise

---
+++ lib/guessing_game.ex
@@ -0,0 +1,18 @@
+defmodule GuessingGame do
+  @spec is_close_number(integer(), integer()) :: boolean()
+  defguard is_close_number(secret_number, guess)
+           when secret_number + 1 === guess or secret_number - 1 === guess
+
+  @spec compare(integer(), integer() | atom()) :: String.t()
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+  def compare(secret_number, guess) when is_close_number(secret_number, guess), do: "So close"
+
+  def compare(secret_number, guess) when is_integer(guess) and secret_number - 1 > guess,
+    do: "Too low"
+
+  def compare(secret_number, guess) when is_integer(guess) and guess > secret_number + 1,
+    do: "Too high"
+
+  def compare(_secret_number, _guess), do: "Make a guess"
+end
