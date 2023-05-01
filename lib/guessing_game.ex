22c0a132109dbb7b210c84b2a1d8cbfd7f1db4c1
Nitesh Mishra <20892499+niteshcode4u@users.noreply.github.com>
2023-05-01 09:26:54 +0530

    refactor guessing game logic (#14)
    * refactor guessing game logic
    ---------
    
    Co-authored-by: Nitesh Mishra <>

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -64,19 +64,11 @@ defmodule GuessingGame do
     # => "Make a guess"
     ```
   """
-  defguardp is_close_number(secret_number, guess)
-            when secret_number + 1 === guess or secret_number - 1 === guess
-
-  @spec compare(integer(), integer() | atom()) :: String.t()
+  @spec compare(integer(), any) :: String.t()
   def compare(secret_number, guess \\ :no_guess)
-  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
-  def compare(secret_number, guess) when is_close_number(secret_number, guess), do: "So close"
-
-  def compare(secret_number, guess) when is_integer(guess) and secret_number - 1 > guess,
-    do: "Too low"
-
-  def compare(secret_number, guess) when is_integer(guess) and guess > secret_number + 1,
-    do: "Too high"
-
-  def compare(_secret_number, _guess), do: "Make a guess"
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 0, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
 end
