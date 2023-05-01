90bec76128cf2400caef54783d9653746a21cb10
Nitesh Mishra <>
2023-05-01 09:25:56 +0530

    fixes failing test case

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -64,13 +64,11 @@ defmodule GuessingGame do
     # => "Make a guess"
     ```
   """
-  defmodule GuessingGame do
-    @spec compare(integer(), any) :: String.t()
-    def compare(secret_number, guess \\ :no_guess)
-    def compare(_secret_number, :no_guess), do: "Make a guess"
-    def compare(secret_number, guess) when abs(secret_number - guess) == 0, do: "Correct"
-    def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
-    def compare(secret_number, guess) when secret_number > guess, do: "Too low"
-    def compare(secret_number, guess) when secret_number < guess, do: "Too high"
-  end
+  @spec compare(integer(), any) :: String.t()
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 0, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
 end
